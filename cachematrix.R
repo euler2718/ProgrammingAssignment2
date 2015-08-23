79e7bc6842a4e294c9f68f7314c6c2115d5e4054
euler2718 <euler2718@users.noreply.github.com>
2015-08-23 16:18:47 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,35 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        mat <- NULL
+        write <- function(y) {
+                x <<- y 
+                mat <<- NULL
+        }
+        read <- function() x ## return the matrix x
+        invwrite <- function(inv) mat <<- inv ## set the cache mat equal
+        ## to the inverse of the matrix x
+        readinverse <- function() mat ## return the cached inverse of x
+        list(write = write, read = read,
+             invwrite = invwrite,
+             invread = invread)
+}
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         ## Return a matrix that is the inverse of 'x'
+        mat <- x$readinv()
+        if(!is.null(mat)) {
+                message("get data from cache ")
+                return(mat)
+        }
+        data <- x$get()
+        mat <- solve(data, ...)
+        x$writeinv(mat)
+        mat
+}
 }
